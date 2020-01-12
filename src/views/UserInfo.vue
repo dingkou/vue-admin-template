d35fc6231fff7ec053d3c99db8d193eba93dfeaf
dingkou <dingkoko@163.com>
2020-01-12 23:55:31 +0800

    init

---
+++ src/views/UserInfo.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <Table border ref="selection" :columns="columns4" :data="data1">
+            <template slot-scope="{ row, index }" slot="action">
+            <Button type="primary" size="small" style="margin-right: 5px" @click="show(index)">View</Button>
+            <Button type="error" size="small" @click="remove(index)">Delete</Button>
+        </template>
+        </Table>
+        <Button @click="handleSelectAll(true)">Set all selected</Button>
+        <Button @click="handleSelectAll(false)">Cancel all selected</Button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'userinfo',
+    data () {
+        return {
+            columns4: [
+                {
+                    type: 'selection',
+                    width: 60,
+                    align: 'center'
+                },
+                {
+                    title: 'Name',
+                    key: 'name'
+                },
+                {
+                    title: 'Age',
+                    key: 'age'
+                },
+                {
+                    title: 'Address',
+                    key: 'address'
+                },
+                {
+                    title: 'Action',
+                    slot: 'action',
+                    width: 150,
+                    align: 'center'
+                }
+            ],
+            data1: [
+                {
+                    name: 'John Brown',
+                    age: 18,
+                    address: 'New York No. 1 Lake Park',
+                    date: '2016-10-03'
+                },
+                {
+                    name: 'Jim Green',
+                    age: 24,
+                    address: 'London No. 1 Lake Park',
+                    date: '2016-10-01'
+                },
+                {
+                    name: 'Joe Black',
+                    age: 30,
+                    address: 'Sydney No. 1 Lake Park',
+                    date: '2016-10-02'
+                },
+                {
+                    name: 'Jon Snow',
+                    age: 26,
+                    address: 'Ottawa No. 2 Lake Park',
+                    date: '2016-10-04'
+                }
+            ]
+        }
+    },
+    methods: {
+        handleSelectAll (status) {
+            this.$refs.selection.selectAll(status);
+        }
+    }
+}
+
+
+
+// export default {
+//     name: 'userinfo',
+//     data() {
+//         return {
+//
+//         }
+//     },
+//
+// }
+</script>
+
+<style scoped>
+
+</style>
